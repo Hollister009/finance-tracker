4516eac69d3ecc8baf24dd6bd7a14796a3bc5d86
Andrew Zhukevych <34597767+Hollister009@users.noreply.github.com>
2021-07-31 23:29:28 +0300

    Add currency formatting functions (#11)
    * Add currency formatting functions
    
    * Small adjustments

---
+++ src/utils/index.ts
@@ -0,0 +1,36 @@
+import { CURRENCY_SYMBOLS } from '../constants';
+import { CurrencyCode } from '../types';
+
+const formatter = new Intl.NumberFormat('en-US', { style: 'decimal' });
+
+type FormatInput = {
+  value: number;
+  currency?: CurrencyCode;
+};
+
+const monoFormatterRe = /([,])|([.])/g;
+const monoFormatterReReplacer = (match: string, $1?: string, $2?: string) => {
+  if ($1 === ',') return ' ';
+  if ($2 === '.') return ',';
+  return match;
+};
+
+// Monobank formatting style
+// 2735.5 -> 2 735,5 ₴
+export const monoFormatCurrency = ({
+  value,
+  currency = 'UAH',
+}: FormatInput): string =>
+  `${formatter
+    .format(value)
+    .replace(monoFormatterRe, monoFormatterReReplacer)} ${
+    CURRENCY_SYMBOLS[currency]
+  }`;
+
+// Regular formatting style
+// 49778 -> $49,778
+export const regularFormatCurrency = ({
+  value,
+  currency = 'UAH',
+}: FormatInput): string =>
+  `${CURRENCY_SYMBOLS[currency]}${formatter.format(value)}`;
