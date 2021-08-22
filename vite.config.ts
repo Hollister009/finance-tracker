bc032bb30bd0f09c876778ab564ffef45defce83
Hollister009 <reactive.andrew@yahoo.com.ua>
2021-08-22 21:53:01 +0300

    Implemented fonts loading functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import ViteFonts from 'vite-plugin-fonts';
+
+const fontsConfig = ViteFonts({
+  google: {
+    families: ['Inter'],
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()]
-})
+  plugins: [reactRefresh(), tsconfigPaths(), fontsConfig],
+});
